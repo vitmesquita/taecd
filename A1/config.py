a746f18933720a02439d423081851e13dd1e6934
vitmesquita <vitoria.leite.ismart@gmail.com>
2021-06-19 21:31:59 -0300

    initial commit

---
+++ A1/config.py
@@ -0,0 +1,7 @@
+import os
+
+path_dados = r'C:\Users\Olá\OneDrive - Fundacao Getulio Vargas - FGV\kim\taecd\A1\bases'
+
+path_part1 = os.path.join(path_dados,'Part 1.tsv')
+
+path_part2 = os.path.join(path_dados,'Part 2.tsv')
